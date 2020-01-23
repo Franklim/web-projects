7944959bcd52bde69788df9e371294c84ae53c48
Paulo Franklim <paulofranklim@hotmail.com>
2020-01-22 23:45:48 -0300

    First part of input form

--- react-projects/frontend-project/src/App.js
+++ react-projects/frontend-project/src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import './global.css';
+import './App.css';
+import './SideBar.css'
 
 function App() {
   return (
-    <div className="App"></div>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github">Github</label>
+            <input name="github" id="github" />
+          </div>
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" />
+            </div>
+
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
   );
 }
 
