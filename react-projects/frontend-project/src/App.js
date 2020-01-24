f8e16d7b1344f638a9aeab6722b7e56192dfba61
Paulo Franklim <paulofranklim@hotmail.com>
2020-01-23 22:31:41 -0300

    Version 2 from input form

--- react-projects/frontend-project/src/App.js
+++ react-projects/frontend-project/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './global.css';
 import './App.css';
 import './SideBar.css'
-
+import './Main.css'
 function App() {
   return (
     <div id="app">
@@ -33,7 +33,54 @@ function App() {
           <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main></main>
+      <main>
+        <ul>
+        <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/9970061?s=460&v=4" alt="Franklim"/>
+                <div className="user-info">
+                  <strong>Paulo Franklim</strong>
+                  <span>Java, Node.js</span>
+                  <p>Biografia</p>
+                  <a href="https://github.com/franklim">Acessar perfil no Github</a>
+                </div>
+              </header>
+          </li>
+          <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/9970061?s=460&v=4" alt="Franklim"/>
+                <div className="user-info">
+                  <strong>Paulo Franklim</strong>
+                  <span>Java, Node.js</span>
+                  <p>Biografia</p>
+                  <a href="https://github.com/franklim">Acessar perfil no Github</a>
+                </div>
+              </header>
+          </li>
+          <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/9970061?s=460&v=4" alt="Franklim"/>
+                <div className="user-info">
+                  <strong>Paulo Franklim</strong>
+                  <span>Java, Node.js</span>
+                  <p>Biografia</p>
+                  <a href="https://github.com/franklim">Acessar perfil no Github</a>
+                </div>
+              </header>
+          </li>
+          <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/9970061?s=460&v=4" alt="Franklim"/>
+                <div className="user-info">
+                  <strong>Paulo Franklim</strong>
+                  <span>Java, Node.js</span>
+                  <p>Biografia</p>
+                  <a href="https://github.com/franklim">Acessar perfil no Github</a>
+                </div>
+              </header>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
