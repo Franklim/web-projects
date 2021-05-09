626326eb3a792bd52969006efe5f855099b2d6df
Paulo Franklim <paulofranklim@hotmail.com>
2021-05-08 22:20:14 -0300

    Including endpoints to get one user and list of all users

--- java-projects/lotterychecker/src/main/java/com/lotterychecker/controller/UserController.java
+++ java-projects/lotterychecker/src/main/java/com/lotterychecker/controller/UserController.java
@@ -3,9 +3,12 @@
  */
 package com.lotterychecker.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,6 +44,24 @@ public class UserController {
 	LOG.debug(savedUser.toString());
 	LOG.debug("Exit method saveUser(@RequestBody User user)");
 	return "User '" + savedUser.getName() + "' created. id=" + savedUser.getId();
+    }
+    
+    @RequestMapping(value = "load-user/{id}", method = RequestMethod.GET)
+    public User loadtUser(@PathVariable("id") String id) {
+	LOG.debug("Entry method getUser(@PathVariable(\"id\") String id) ");
+	
+	User user = service.getUser(id);
+	LOG.debug("Exit method getUser(@PathVariable(\"id\") String id)");
+	return user;
+    }
+
+    @RequestMapping(value = "load-users", method = RequestMethod.GET)
+    public List<User> loadAllUsers() {
+	LOG.debug("Entry method getAllUsers()");
+	
+	List<User> users = service.getAllUsers();
+	LOG.debug("Exit method getAllUsers()");
+	return users;
     }
     
 }
